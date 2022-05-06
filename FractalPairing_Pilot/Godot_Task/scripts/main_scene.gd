6085b2b60d47c540829f61b9ee7d211249d40b11
ffviana <92690590+ffviana@users.noreply.github.com>
2022-05-06 19:35:40 +0100

    Add files via upload

--- FractalPairing_Pilot/Godot_Task/scripts/main_scene.gd
+++ FractalPairing_Pilot/Godot_Task/scripts/main_scene.gd
@@ -1,34 +1,66 @@
 extends Node2D
 
-# Declare member variables here. Examples:
+# Create other file to save missed attempts
+
 var ans: int
-var answers: Array
-# var b = "text"
+var sub_id: String
+var sub_dataPath: String
+var answers: Dictionary
+
+var Fractal_misPress = []
+var Trial = 0
+var ChoiceTimestamp = OS.get_unix_time()
+
+var dateTime = OS.get_datetime()
+var year = dateTime.year;
+var month = dateTime.month;
+var day = dateTime.day;
+var hour = dateTime.hour;
+var minute = dateTime.minute;
+
+
+# Called whenever a Fractal is pressed
+func _Fractal_pressed(which):
+	# Holds answer for until confirmation
+	ans = which.get_index() 
+	ChoiceTimestamp = OS.get_unix_time()
+	
+	# Goes through Fractal Buttons
+	for i in $Fractals.get_children():
+		if i != which:
+			i.get_child(0).set_frame(1) # Changes Fractal State
+	fractalDisable()
+	popupConfirmation()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ITI.visible = true
-	$Fractals.visible = false
+	$SubjectID.visible = true
+	answers = {'User': [],'Trial': [], 'Fractal misPress':[],'Choice Timestamp': [], 'Confirmation Timestamp': [],'Value': []}
+	$IntroMessage.visible = false
+	$ITI/Background.visible = true
 	$ITI/RichTextLabel.visible = false
-
-#	for i in group.get_buttons():
-#		i.connect("pressed",self,"button_pressed")
+	# Connect all Fractal buttons 
+	for b in $Fractals.get_children(): 
+		b.connect("pressed", self, "_Fractal_pressed",[b])
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Esc to Exit Program
 	if Input.is_action_pressed("ui_cancel"):
-		save(answers)
+		save(answers, sub_dataPath)
 		get_tree().quit()
 
-func save(content):
+func save(content, sub_id):
 	var file = File.new()
-	file.open("res://save_exp.json", File.WRITE) # Change NAME to somessing usefol
-	file.store_line(JSON.print(content))
+	file.open( str(sub_id), File.WRITE) # Change NAME to somessing usefol
+	file.store_line(to_json(content))
 	file.close()
 
 func popupConfirmation():
 	$TEXT1.visible = false
 	$ConfirmationQuestion.visible = true
+	fractalDisable()
 
 func startITI():
 	$Fractals.visible = false
@@ -41,74 +73,43 @@ func startTrial():
 	$ConfirmationQuestion.visible = false
 	$Fractals.visible = true
 	$TEXT1.visible = true
-	$Fractals/Fractal1/state.set_frame(0)
-	$Fractals/Fractal2/state.set_frame(0)
-	$Fractals/Fractal3/state.set_frame(0)
-	$Fractals/Fractal4/state.set_frame(0)
-	$Fractals/Fractal5/state.set_frame(0)
-	$Fractals/Fractal6/state.set_frame(0)
+	fractalEnable()
 
-func _on_Fractal1_pressed():
-	ans = 1
-	popupConfirmation()
-	$Fractals/Fractal2/state.set_frame(1)
-	$Fractals/Fractal3/state.set_frame(1)
-	$Fractals/Fractal4/state.set_frame(1)
-	$Fractals/Fractal5/state.set_frame(1)
-	$Fractals/Fractal6/state.set_frame(1)
+func fractalDisable():
+	for i in $Fractals.get_children():
+		i.disabled = true
 
-func _on_Fractal2_pressed():
-	ans = 2
-	popupConfirmation()
-	$Fractals/Fractal1/state.set_frame(1)
-	$Fractals/Fractal3/state.set_frame(1)
-	$Fractals/Fractal4/state.set_frame(1)
-	$Fractals/Fractal5/state.set_frame(1)
-	$Fractals/Fractal6/state.set_frame(1)
-
-func _on_Fractal3_pressed():
-	ans = 3
-	popupConfirmation()
-	$Fractals/Fractal1/state.set_frame(1)
-	$Fractals/Fractal2/state.set_frame(1)
-	$Fractals/Fractal4/state.set_frame(1)
-	$Fractals/Fractal5/state.set_frame(1)
-	$Fractals/Fractal6/state.set_frame(1)
-
-func _on_Fractal4_pressed():
-	ans = 4
-	popupConfirmation()
-	$Fractals/Fractal1/state.set_frame(1)
-	$Fractals/Fractal2/state.set_frame(1)
-	$Fractals/Fractal3/state.set_frame(1)
-	$Fractals/Fractal5/state.set_frame(1)
-	$Fractals/Fractal6/state.set_frame(1)
-
-func _on_Fractal5_pressed():
-	ans = 5
-	popupConfirmation()
-	$Fractals/Fractal1/state.set_frame(1)
-	$Fractals/Fractal2/state.set_frame(1)
-	$Fractals/Fractal3/state.set_frame(1)
-	$Fractals/Fractal4/state.set_frame(1)
-	$Fractals/Fractal6/state.set_frame(1)
-
-func _on_Fractal6_pressed():
-	ans = 6
-	popupConfirmation()
-	$Fractals/Fractal1/state.set_frame(1)
-	$Fractals/Fractal2/state.set_frame(1)
-	$Fractals/Fractal3/state.set_frame(1)
-	$Fractals/Fractal4/state.set_frame(1)
-	$Fractals/Fractal5/state.set_frame(1)
-
+func fractalEnable():
+	for i in $Fractals.get_children():
+		i.disabled = false
+		i.get_child(0).set_frame(0)
 
 func _on_Yes_pressed():
-	answers.append(ans)
+	Trial += 1
+	answers["User"].push_back(Trial)
+	answers["Trial"].push_back(Trial)
+	answers["Fractal misPress"].push_back(Fractal_misPress)
+	answers["Choice Timestamp"].push_back(OS.get_unix_time())
+	answers["Confirmation Timestamp"].push_back(OS.get_unix_time())
+	answers["Value"].push_back(ans)
 	startITI()
 
 func _on_No_pressed():
+	Fractal_misPress.append(ans)
 	startTrial()
 
 func _on_fixationButton_pressed():
 	startTrial()
+
+func _on_LineEdit_text_entered(new_text):
+	sub_id = "ercffa_%03d" % int(new_text)
+	sub_dataPath = "user://%s_%02d-%02d-%s_%02d-%02d.json" % [sub_id, day, month, year, hour, minute]
+	print(sub_dataPath)
+	$ITI.visible = true
+	$IntroMessage.visible = true
+	$Fractals.visible = false
+	$ITI/RichTextLabel.visible = false
+	$SubjectID.visible = false
+
+
+
